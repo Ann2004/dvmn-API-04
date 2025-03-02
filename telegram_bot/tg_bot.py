115cb14b722573a26f9fba5e22ea7b73a67deea1
nyutdf2 <annslob@gmail.com>
2025-03-02 13:21:58 +0300

    Update tg_bot.py

--- telegram_bot/tg_bot.py
+++ telegram_bot/tg_bot.py
@@ -1,17 +1,19 @@
 import telegram
 import os
 from dotenv import load_dotenv
+import random
 
 
-def main():
+def send_photo_to_chat(file_path=None):
     load_dotenv()
     tg_token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=tg_token)
     
-    chat_id = '@cosmosDvmn'
-    bot.send_document(chat_id=chat_id, document='https://python-telegram-bot.org/static/testfiles/telegram.gif')
-
-
-if __name__ == '__main__':
-    main()
+    chat_id = os.environ['TG_CHAT_ID']
     
+    if file_path is None:
+        directory = os.path.join('images')
+        file_paths = [os.path.join(directory, filename) for filename in os.listdir(directory)]
+        file_path = random.choice(file_paths)
+           
+    bot.send_document(chat_id=chat_id, document=open(file_path, 'rb'))   
