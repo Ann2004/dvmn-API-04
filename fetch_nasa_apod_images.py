4c789b78e0a3d9d14732c54c61b409eeee34bc52
nyutdf2 <annslob@gmail.com>
2025-03-05 20:43:53 +0300

    Update fetch_nasa_apod_images.py

--- fetch_nasa_apod_images.py
+++ fetch_nasa_apod_images.py
@@ -2,21 +2,18 @@ import requests
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import get_file_extension
+import download_and_save_image
 
 
-def fetch_nasa_apod(url, payload):
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
+def fetch_nasa_apod(nasa_token, posts_amount=15):
+    nasa_apod_url = 'https://api.nasa.gov/planetary/apod'
+    payload = {'api_key': nasa_token, 'count': posts_amount}
+    response = requests.get(nasa_apod_url, params=payload)
+    response.raise_for_status() 
     
     for index, image in enumerate(response.json(), start=1):
-        image_response = requests.get(image['url'])
-        image_response.raise_for_status()
-        
-        extension = get_file_extension.get_file_extension(image['url'])
-        
-        with open(f'images/nasa_apod_{index}{extension}', 'wb') as file:
-            file.write(image_response.content)
+        name = 'nasa_apod_'
+        download_and_save_image.download_and_save_image(image['url'], index, name)
 
 
 def main():
@@ -25,9 +22,7 @@ def main():
     
     Path("images").mkdir(parents=True, exist_ok=True)
     
-    payload_apod = {'api_key': nasa_token, 'count': '30'}
-    nasa_apod_url = 'https://api.nasa.gov/planetary/apod'
-    fetch_nasa_apod(nasa_apod_url, payload_apod)
+    fetch_nasa_apod(nasa_token)
     
     
 if __name__ == '__main__':
