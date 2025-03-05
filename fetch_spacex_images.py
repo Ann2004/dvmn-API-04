b6d31febdf2c58044ff075c0a45501fcad67ea62
nyutdf2 <annslob@gmail.com>
2025-03-05 20:41:08 +0300

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -1,7 +1,7 @@
 import requests
 from pathlib import Path
 import argparse
-import get_file_extension
+import download_and_save_image
 
 
 def fetch_spacex_last_launch(url):
@@ -10,33 +10,22 @@ def fetch_spacex_last_launch(url):
     image_urls = response.json()['links']['flickr']['original']
     
     for index, image_url in enumerate(image_urls, start=1):
-        image_response = requests.get(image_url)
-        image_response.raise_for_status()
-        
-        extension = get_file_extension.get_file_extension(image_url['url'])
-        
-        with open(f'images/spacex{index}{extension}', 'wb') as file:
-            file.write(image_response.content)
+        name = 'spacex'
+        download_and_save_image.download_and_save_image(image_url, index, name)
         
 
 def main():
     parser = argparse.ArgumentParser(description='download photos from SpaceX')
-    parser.add_argument('-id', '--launch_id', help='Launch ID')
+    parser.add_argument('-id', '--launch_id', help='Launch ID', default='latest')
     args = parser.parse_args()
     launch_id = args.launch_id
     
     Path("images").mkdir(parents=True, exist_ok=True)
     
-    if launch_id:
-        spacex_url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
-    else:
-        spacex_url = 'https://api.spacexdata.com/v5/launches/latest'
+    spacex_url = f'https://api.spacexdata.com/v5/launches/{launch_id}'
     
     fetch_spacex_last_launch(spacex_url)
     
     
 if __name__ == '__main__':
     main()
-    
-#6243adcaaf52800c6e919254 - launch id
-#61fc0203e0dc5662b76489a8  - launch id
