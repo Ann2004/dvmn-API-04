8ddda87f579a77c135ddb5c853a9f4a7ac86e9f2
nyutdf2 <annslob@gmail.com>
2025-03-05 20:44:48 +0300

    Update post_photos_to_telegram.py

--- post_photos_to_telegram.py
+++ post_photos_to_telegram.py
@@ -4,24 +4,24 @@ from telegram_bot.tg_bot import send_photo_to_chat
 import os
 import time
 import random
+from dotenv import load_dotenv
 
 
 def main():
-    delay_hours = 4
+    load_dotenv()
+    tg_token = os.environ['TG_TOKEN']
+    chat_id = os.environ['TG_CHAT_ID']
     
     parser = argparse.ArgumentParser(description='Post photos to Telegram')
-    parser.add_argument('-H', '--hours', help='publication frequency - number delay hours', type=int)
+    parser.add_argument('-H', '--hours', help='publication frequency - number delay hours', type=int, default=4)
     args = parser.parse_args()
-    if args.hours:
-        delay_hours = args.hours
     
     directory = Path('images')
     file_paths = [os.path.join(directory, filename) for filename in os.listdir(directory)]  
     while True:
         for file_path in file_paths:
-            print(file_path)
-            send_photo_to_chat(file_path)
-            time.sleep(delay_hours * 3600)
+            send_photo_to_chat(tg_token, chat_id, file_path)
+            time.sleep(args.hours * 3600)
         random.shuffle(file_paths)
             
 
