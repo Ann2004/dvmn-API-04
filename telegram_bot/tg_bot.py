c242836dcd3c942d9d79beaa0b05bfa116b0244c
nyutdf2 <annslob@gmail.com>
2025-03-07 21:02:50 +0300

    Update tg_bot.py

--- telegram_bot/tg_bot.py
+++ telegram_bot/tg_bot.py
@@ -1,7 +1,6 @@
 import telegram
-import os
-import random
 import logging
+from get_downloaded_files import get_downloaded_files
 
 
 logger = logging.getLogger('tg_bot')
@@ -11,9 +10,7 @@ def send_photo_to_chat(tg_token, chat_id, file_path=None):
     bot = telegram.Bot(token=tg_token)
     
     if not file_path:
-        directory = os.path.join('images')
-        file_paths = [os.path.join(directory, filename) for filename in os.listdir(directory)]
-        file_path = random.choice(file_paths)
+        file_path = get_downloaded_files('images')
     
     logger.info(f'Sending photo: {file_path}')
     
